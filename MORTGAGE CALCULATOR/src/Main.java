718f335acbe75754c7e1ced12b751859d46c5eb8
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-05-26 01:43:57 +0300

    Concept working calculator

--- MORTGAGE CALCULATOR/src/Main.java
+++ MORTGAGE CALCULATOR/src/Main.java
@@ -4,17 +4,24 @@ void main() {
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Principal (1K - 1M):");
+    System.out.print("Principal (1K - 1M):");
     int principal = scanner.nextInt();
     System.out.println("So principal is :" + principal);
 
-    System.out.println("Annual Interest Rate:");
-    float annualInterestRate = (float) scanner.nextFloat();
+    System.out.print("Annual Interest Rate:");
+    double annualInterestRate =  scanner.nextDouble();
     System.out.println("So annual Interest Rate is :" + annualInterestRate);
+    double monthlyInterestRate =  annualInterestRate/percetage/numberOfMonthPerYear;
 
-    System.out.println("Period (Years)");
+    System.out.print("Period (Years)");
     int period = scanner.nextInt();
     System.out.println("So period is :" + period);
 
-    double
+    int numberOfPayments = numberOfMonthPerYear*period;
+
+    double numerator = monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments);
+    double denominator = Math.pow(1+monthlyInterestRate,numberOfPayments) - 1;
+    double Mortgage = principal * (numerator/denominator);
+
+    System.out.println("Mortgage is :" + Mortgage);
 }
