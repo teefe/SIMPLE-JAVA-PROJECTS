d42597626c768603ddd47f89c299c1fcab19f5ef
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-05-26 23:20:44 +0300

    Implemanted the control flow error handling

--- MORTGAGE CALCULATOR/src/Main.java
+++ MORTGAGE CALCULATOR/src/Main.java
@@ -1,27 +1,50 @@
-void main() {
+void main(String[] args) {
     final byte numberOfMonthPerYear =12;
-    final byte percetage = 100;
+    final byte percentage = 100;
+    int principal = 0;
+    double annualInterestRate = 0.0;
+    int period = 0;
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Principal (1K - 1M):");
-    int principal = scanner.nextInt();
-    System.out.println("So principal is :" + principal);
+    while (true) {
+        System.out.print("Principal (UGX1K - UGX1M):");
+        int clientValue = scanner.nextInt();
+        if  (clientValue >= 1000 && clientValue <= 1000000) {
+            principal = clientValue;
+            System.out.println("So principal is :" + principal);
+            break;
+        }else
+            System.out.println("Enter value 1K and 1M");
+    }
 
-    System.out.print("Annual Interest Rate:");
-    double annualInterestRate =  scanner.nextDouble();
-    System.out.println("So annual Interest Rate is :" + annualInterestRate);
-    double monthlyInterestRate =  annualInterestRate/percetage/numberOfMonthPerYear;
+    while (true) {
+        System.out.print("Annual Interest Rate:");
+        double clientValue =  scanner.nextDouble();
+        if (clientValue >= 0 && clientValue <= 30){
+            annualInterestRate = clientValue;
+            System.out.println("So annual Interest Rate is :" + clientValue);
+            break;
+        }else
+            System.out.println("Enter value 0 and 30");
+    }
+    while (true) {
+        System.out.print("Period (Years):");
+        int clientValue = scanner.nextInt();
+        if (clientValue >= 1 && clientValue <= 30){
+            period = clientValue;
+            System.out.println("So period is :" + period);
 
-    System.out.print("Period (Years)");
-    int period = scanner.nextInt();
-    System.out.println("So period is :" + period);
+            break;
+        }else
+            System.out.println("Enter value 1 or 30");
+    }
 
     int numberOfPayments = numberOfMonthPerYear*period;
-
+    double monthlyInterestRate =  annualInterestRate/ percentage /numberOfMonthPerYear;
     double numerator = monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments);
     double denominator = Math.pow(1+monthlyInterestRate,numberOfPayments) - 1;
     double Mortgage = principal * (numerator/denominator);
 
-    System.out.println("Mortgage is :" + Mortgage);
+    System.out.println("Mortgage is :" + NumberFormat.getCurrencyInstance().format(Mortgage));
 }
