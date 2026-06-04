0ee84ef81864a13149c3f12d4ec31c259ce7c1a1
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2026-06-04 23:07:11 +0300

    reogarnized using methods

--- MORTGAGE CALCULATOR/src/Main.java
+++ MORTGAGE CALCULATOR/src/Main.java
@@ -5,52 +5,56 @@ void main(String[] args) {
     double annualInterestRate = 0.0;
     int period = 0;
 
-    while (true) {
-        System.out.print("Principal (UGX1K - UGX1M):");
-        Scanner scanner = new Scanner(System.in);
-        int clientValue = scanner.nextInt();
-        if  (clientValue >= 1000 && clientValue <= 1000000) {
-            principal = clientValue;
-            System.out.println("So principal is :" + principal);
-            break;
-        }else
-            System.out.println("Enter value 1K and 1M");
-    }
-
-    while (true) {
-        System.out.print("Annual Interest Rate:");
-        Scanner scanner = new Scanner(System.in);
-        double clientValue =  scanner.nextDouble();
-        if (clientValue >= 0 && clientValue <= 30){
-            annualInterestRate = clientValue;
-            System.out.println("So annual Interest Rate is :" + clientValue);
-            break;
-        }else
-            System.out.println("Enter value 0 and 30");
-    }
-
-    while (true) {
-        System.out.print("Period (Years):");
-        Scanner scanner = new Scanner(System.in);
-        int clientValue = scanner.nextInt();
-        if (clientValue >= 1 && clientValue <= 30){
-            period = clientValue;
-            System.out.println("So period is :" + period);
-
-            break;
-        }else
-            System.out.println("Enter value 1 or 30");
-    }
+    principal = clientInputPrincipal();
+    annualInterestRate = clientInputAnnualInterestRate();
+    period = clientInputPeriod();
 
     int numberOfPayments = numberOfMonthPerYear*period;
     double monthlyInterestRate =  annualInterestRate/ percentage /numberOfMonthPerYear;
-    mortgageCalculator(principal,monthlyInterestRate,numberOfPayments);
+    System.out.println(mortgageCalculator(principal,monthlyInterestRate,numberOfPayments));
 }
 
-public void mortgageCalculator(int principal,double monthlyInterestRate,int numberOfPayments){
+public double mortgageCalculator(int principal,double monthlyInterestRate,int numberOfPayments){
     double numerator = monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments);
     double denominator = Math.pow(1+monthlyInterestRate,numberOfPayments) - 1;
-    double Mortgage = principal * (numerator/denominator);
-    System.out.println("Mortgage is :" + NumberFormat.getCurrencyInstance().format(Mortgage));
-   // return Mortgage;
+    return principal * (numerator/denominator);
 }
+
+public int clientInputPeriod(){
+    while (true) {
+        System.out.print("Period (Years):");
+        Scanner scanner = new Scanner(System.in);
+        int inputPeriod = scanner.nextInt();
+        if (inputPeriod >= 1 && inputPeriod <= 30){
+            System.out.println("So period is :" + inputPeriod);
+            return inputPeriod;
+        }else
+            System.out.println("Enter value 1 or 30");
+    }
+}
+public int clientInputPrincipal(){
+    while (true) {
+        System.out.print("Principal (UGX1K - UGX1M):");
+        Scanner scanner = new Scanner(System.in);
+        int inputPrincipal = scanner.nextInt();
+        if  (inputPrincipal >= 1000 && inputPrincipal <= 1000000) {
+            System.out.println("So principal is :" + inputPrincipal);
+            return inputPrincipal;
+        }else
+            System.out.println("Enter value 1K and 1M");
+    }
+}
+public double clientInputAnnualInterestRate(){
+    while (true) {
+        System.out.print("Annual Interest Rate:");
+        Scanner scanner = new Scanner(System.in);
+        double inputAnnualInterestRate =  scanner.nextDouble();
+        if (inputAnnualInterestRate >= 0 && inputAnnualInterestRate <= 30){
+            System.out.println("So annual Interest Rate is :" + inputAnnualInterestRate);
+            return inputAnnualInterestRate;
+        }else
+            System.out.println("Enter value 0 and 30");
+    }
+}
+
+
